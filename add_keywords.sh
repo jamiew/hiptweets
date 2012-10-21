3a89a008d790b1222a77386cca52a137025f1c47
Jamie Wilkinson <jamie@jamiedubs.com>
2012-10-20 22:22:04 -0700

    Shortcut scripts for adding search terms and users

---
+++ add_keywords.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+  echo "Specify search keyword(s) to add, comma-delimited, with no spaces"
+  exit 1
+fi
+
+var="HIPTWEET_SEARCH"
+current=$(heroku config:get $var)
+
+echo "Status quo:"
+echo "$var: $current"
+
+heroku config:set $var="$current,$1" || exit $?
+
+echo "Done."
